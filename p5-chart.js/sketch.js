0124152e853b6f419596aac6e3c236423e31255f
H-4-N-5 <hansheidmann@gmail.com>
2015-12-08 23:16:51 -0700

    updates

---
+++ p5-chart.js/sketch.js
@@ -0,0 +1,107 @@
+//  DDE Voting Booth Dynamic Pie Chart 
+//  Hans Heidmann 2015
+
+
+var r,g,b,a; // for my tasty pie's slice colors...
+
+var candidates = ["Hillary Clinton",  
+                  "Bernie Sanders",   
+                  "Martin O’mally",   
+                  "Donald Trump", 
+                  "Ben Carson",    
+                  "Marco Rubio"
+                  ]
+                  
+var votes = [0,0,0,0,0,0];
+var randomVote = 0; 
+var totalVotes = 0;
+
+var lastAngle = 0;
+
+var angles = [60,60,60,60,60,60];
+
+
+
+function setup() {
+  canvas = createCanvas(700, 500);
+  canvas.position(50, 50);
+  //canvas.parent("p5-container");
+  
+  button = createButton('generate random vote');
+  button.position(canvas.x+ 20, canvas.y + height - 20);
+  button.mousePressed(createRandomVote);
+
+  noLoop();
+}
+
+function draw() {
+  
+  strokeWeight(3);
+  stroke(0);
+  
+  background(0);
+
+  fill(random(255));
+  text(candidates, 10,10);
+  text(votes, 10,25);
+  text("totalVotes: " + totalVotes, 10,40);
+  text("randomVote:" + randomVote, 10,55);
+  
+  
+  if (totalVotes == 0) {
+      fill(255);
+      ellipse(width/2, height/2, 200, 200);
+  }
+  
+  else if (totalVotes == 1) {
+    for(var v=0; v<6; v++) {
+      if(votes[v] == 1) {
+        if (v<3) {
+          fill(255,0,0);
+          ellipse(width/2, height/2, 200, 200);
+          break;
+        } else {
+          fill(0,0,255);
+          ellipse(width/2, height/2, 200, 200);
+        }
+      }
+    }
+    
+  } 
+  
+  else if (totalVotes >= 2) {
+    for (var i = 0; i < 6; i++) { // data.length = 6 (6 candidates)
+      
+      arc(width/2, height/2, 200, 200, 0, lastAngle+radians(angles[0]));
+      lastAngle += radians(angles[i]);
+      
+      if (i < 3) {
+        r = floor(random(0,255));
+        g = floor(random(0,255));
+        b = floor(random(0,255));
+      }
+      
+      var color = (r,g,b);
+      
+      fill(color);
+      arc(width/2, height/2, diameter, diameter, 0, lastAngle+radians(angles[i]));
+      lastAngle += radians(angles[i]);
+      }
+  
+  } 
+  
+
+}
+
+
+function createRandomVote() {
+  randomVote = floor(random(0,6)); 
+  votes[randomVote] = votes[randomVote]+1;
+  totalVotes++;
+  redraw();
+} 
+
+
+
+
+  
