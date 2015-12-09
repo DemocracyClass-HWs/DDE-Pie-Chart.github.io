dc10798a7f65d16c72823ba44b8fcfa260b671b5
H-4-N-5 <hansheidmann@gmail.com>
2015-12-09 02:43:25 -0700

    updates

--- p5-banner.js/sketch.js
+++ p5-banner.js/sketch.js
@@ -1,10 +1,27 @@
 function setup() {
-  //bg = loadImage("assets/moonwalk.jpg");
-  bg = (0);
-  createCanvas(800, 100);
+  //backgroundImage = loadImage("image.jpg");
+  var canvas = createCanvas(900, 100);
+  canvas.parent('p5-banner');
+  
 }
 
 function draw() {
-  background(bg);
+  
+  background(100,160,250);
+  
+  /* In case we want a border
+  stroke(0,255,255);
+  strokeWeight(10);
+  noFill();
+  rect(0,0,width,height);
+  noStroke();
+  */
+  
+  textSize(32);
+  textAlign('center') ;
+  fill(255);
+  var title = "2016 Presidential Election";
+  text(title, width/2, height/2+10);
+  
 
 }
